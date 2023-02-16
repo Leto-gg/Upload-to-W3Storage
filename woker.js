fa196fdfd17014be536f5038fbe8174b34e958bb
Logan Lentz <logan.lentz1@gmail.com>
2023-02-16 07:00:20 -0600

    Create woker.js

---
+++ woker.js
@@ -0,0 +1,24 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const { searchParams } = new URL(request.url)
+  const cid = searchParams.get('cid')
+
+  if (!cid) {
+    return new Response('Missing CID parameter', { status: 400 })
+  }
+
+  // Query Web3.Storage for the CID data
+  const res = await fetch(`https://dweb.link/ipfs/${cid}`)
+  const data = await res.text()
+
+  // Create a new response with the CID data
+  return new Response(data, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
+}
